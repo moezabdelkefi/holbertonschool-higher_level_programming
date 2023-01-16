ebabde0050b1ddf313276ce26c2c6e793f7c2290
root <root@DESKTOP-C0MK0L2.localdomain>
2023-01-16 16:25:33 +0100

    print the letters

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = "Hol"
-word_last_2 = "on"
-middle_word = "olberto"
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
+print(f"First 3 letters: {word [0:3]}")
+print(f"Last 2 letters: {word [-2:]}")
+print(f"Middle word: {word [1:-1]}")
