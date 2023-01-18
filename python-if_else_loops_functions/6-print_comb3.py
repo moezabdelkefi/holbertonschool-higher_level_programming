041fb9b48b76c4aa9d2f39d9960eb448faffcf14
root <root@DESKTOP-C0MK0L2.localdomain>
2023-01-18 18:03:11 +0100

    a program that prints all possible different combinations of two digits

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-smallest_combination = (0, 1)
 for i in range(10):
     for j in range(i+1, 10):
-        if (i, j) < smallest_combination:
-            smallest_combination = (i, j)
-print("{}{}".format(smallest_combination[0], smallest_combination[1]))
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=",")
