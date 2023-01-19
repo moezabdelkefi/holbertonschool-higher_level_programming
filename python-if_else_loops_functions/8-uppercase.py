f9baef932bc2e014439315062e383f5d568b4c6b
root <root@DESKTOP-C0MK0L2.localdomain>
2023-01-19 14:01:35 +0100

    fix the code

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print("{}".format(str.upper()))
+    new_string = ""
+    for char in str:
+        if ord(char) >= ord('a') and ord(char) <= ord('z'):
+            new_string += chr(ord(char) - ord('a') + ord('A')) + ''
+        else:
+            new_string += char + ''
+    print(new_string)
+    print()
