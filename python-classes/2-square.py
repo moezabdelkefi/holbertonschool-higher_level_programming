8121ee9c80622313f37005a3b8738a08404979ec
root <root@DESKTOP-C0MK0L2.localdomain>
2023-01-31 21:52:14 +0100

    a class Square that defines a square

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""define a class square"""
+
+
+class Square:
+    """represent square"""
+
+    def __init__(self, size = 0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
