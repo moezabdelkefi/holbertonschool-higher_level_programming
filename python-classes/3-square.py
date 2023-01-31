e32ab60c0960b6f9db8377b595f50561d8fced11
root <root@DESKTOP-C0MK0L2.localdomain>
2023-01-31 22:12:55 +0100

    a class Square that defines a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""define a class square"""
+
+
+class Square:
+    """represent square"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+       return  self.__size * self.__size
