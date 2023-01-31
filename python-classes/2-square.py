388550e88c6a864f747b2b6157c7223a326f93fa
root <root@DESKTOP-C0MK0L2.localdomain>
2023-01-31 21:56:57 +0100

    a class Square that defines a square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,7 +7,6 @@ class Square:
 
     def __init__(self, size = 0):
         self.__size = size
-
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
