df05fef767cac57328ba1a96ebbab5669398b580
root <root@DESKTOP-C0MK0L2.localdomain>
2023-02-01 13:29:25 +0100

    a class Square that defines a square

---
+++ python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""define a class square"""
+
+
+class Square:
+    """represent square"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        self.__size = value
+
+    def my_print(self):
+        print('\n'.join('#' * self.__size for _ in range(self.__size)))
+        if self.__size == 0:
+            print()
