5a6ff9b2c47b9fc044a16be858158a6bba2f9657
root <root@DESKTOP-C0MK0L2.localdomain>
2023-02-03 19:05:13 +0100

    a class Rectangle that defines a rectangle

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -59,8 +59,9 @@ class Rectangle:
         for i in range(self.__height):
             rectangle.append("#" * self.__width)
         return "\n".join(rectangle)
-    
+
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
+
     def __del__(self):
         print("Bye rectangle...")
