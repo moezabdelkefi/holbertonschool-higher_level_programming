9f8c49b30095ca5da9d6a165ec078a06c3d99afa
root <root@DESKTOP-C0MK0L2.localdomain>
2023-02-03 17:05:11 +0100

    a function that prints a square with the character

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""define a function that divides all elements of a matrix"""
+
+"""
+raises:
+    TypeError: size must be an integer
+    ValueError: size must be >= 0
+    TypeError: size must be an integer
+
+"""
+
+def print_square(size):
+    if size == int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size == float and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(1, size * size + 1):
+        if i % size == 0:
+            print("#")
+        else:
+            print("#", end='')
+
