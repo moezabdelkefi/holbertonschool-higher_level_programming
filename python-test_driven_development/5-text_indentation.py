2129ce15e248f34ff5107c1597bfa768eb5205ce
root <root@DESKTOP-C0MK0L2.localdomain>
2023-02-03 17:37:56 +0100

    a function that prints a text with 2 new lines after each of these characters

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
-"""define a function that prints a 
+
+"""define a function that prints a
 text with 2 new lines after each of these characters """
 
+
 def text_indentation(text):
+
+    """
+    a function that prints a text with 2 new lines
+    after each of these characters
+    typeError: text must be a string
+    """
+
     if type(text) != str:
         raise TypeError("text must be a string")
-    new_text = text.replace(".", ".\n\n").replace("?", "?\n\n").replace(":", ":\n\n")
-    print(new_text.strip(), end="")
+    characters = [".", "?", ":"]
+    for character in characters:
+        text = text.replace(character, character + "\n\n")
+    print(text.strip(), end="")
