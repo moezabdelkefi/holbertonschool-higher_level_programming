2bddb6f2b80e30d698344aad7e0164d606f8422b
root <root@DESKTOP-C0MK0L2.localdomain>
2023-02-03 23:28:38 +0100

    a class Rectangle that defines a rectangle

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -79,6 +79,7 @@ class Rectangle:
     def __del__(self):
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+
     @classmethod
     def square(cls, size=0):
         return cls(size, size)
