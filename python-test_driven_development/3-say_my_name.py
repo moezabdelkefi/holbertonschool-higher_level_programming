ea2f8d7e71c7218cfc8677507e69090658e7bb47
root <root@DESKTOP-C0MK0L2.localdomain>
2023-02-03 16:24:45 +0100

    a function that prints My name is <first name> <last name>

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -6,14 +6,14 @@
 raises:
     TypeError: first_name must be a string
     TypeError: last_name must be a string
-    
 
 """
+
 
 def say_my_name(first_name, last_name=""):
 
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
-    if type(last_name)!= str:
+    if type(last_name) != str:
         raise TypeError("last_name must be a string")
     print("my name is {} {}".format(first_name, last_name))
