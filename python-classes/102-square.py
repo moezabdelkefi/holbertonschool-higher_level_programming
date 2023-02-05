302491c4c5557dfd6c0930b0036da23b4c5d930f
root <root@DESKTOP-C0MK0L2.localdomain>
2023-02-05 19:29:16 +0100

    a class Square that defines a square

--- python-classes/102-square.py
+++ python-classes/102-square.py
@@ -16,43 +16,6 @@ class Square:
     def area(self):
         return self.__size * self.__size
 
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        self.__size = value
-#!/usr/bin/python3
-"""define a class square"""
-
-
-class Square:
-    """represent square"""
-
-    def __init__(self, size=0):
-        self.__size = size
-
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-
-    def area(self):
-        return self.__size * self.__size
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        self.__size = value
-
     def __eq__(self, other):
         return self.area() == other.area()
 
@@ -70,3 +33,13 @@ class Square:
 
     def __le__(self, other):
         return self.area() <= other.area()
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        self.__size = value
