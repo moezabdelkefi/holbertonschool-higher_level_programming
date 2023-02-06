9ef39e76a9cfa5cce5f438edf4320ed881337acb
root <root@DESKTOP-C0MK0L2.localdomain>
2023-02-06 21:57:57 +0100

    aises an Exception with the message

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" define class BaseGeometry"""
+
+
+class BaseGeometry:
+    """creat an empty class"""
+    def area(self):
+        raise Exception("area() is not implemented")
