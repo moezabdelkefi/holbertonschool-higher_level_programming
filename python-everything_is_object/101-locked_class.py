ccc3e138d27ac6b7f8ebe012e528c55fe4be5322
root <root@DESKTOP-C0MK0L2.localdomain>
2023-02-06 13:31:08 +0100

    testing

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-raise AttributeError(" 'LockedClass' object has no attribute 'last_name'")
+
+""" define a class with no class or object attribute"""
+
+
+class LockedClass:
+
+
+    raise AttributeError(" 'LockedClass' object has no attribute 'last_name'")
