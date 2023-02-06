099b4c1b4f55574623e474b7c74db98b9b93c45f
root <root@DESKTOP-C0MK0L2.localdomain>
2023-02-06 21:21:46 +0100

    a function that returns True if the object is exactly an instance of the specified class ; otherwise False

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" define a function"""
+
+
+def is_same_class(obj, a_class):
+    """ a function that returns True if the object is exactly 
+    an instance of the specified class otherwise False"""
+
+    return type(obj) == a_class
