ede2680527ef1b58456c169a249b2c4de0fce642
root <root@DESKTOP-C0MK0L2.localdomain>
2023-02-07 15:17:28 +0100

    a class Rectangle that inherits from BaseGeometry

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" define class BaseGeometry"""
+
+
+class BaseGeometry:
+    """creat an empty class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        self.name = name
+        self.value = value
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """define a class rectangle"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        return None
+
+    def area(self):
+        return self.__height * self.__width
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def __repr(self):
+        return self.__str__()
+
+
+class Square(BaseGeometry):
+    """ a class Square that inherits from Rectangle"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
