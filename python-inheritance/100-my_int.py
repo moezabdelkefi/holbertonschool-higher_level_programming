03bf845c39f3b6c4d9306ecd59c1ce4a49c49691
root <root@DESKTOP-C0MK0L2.localdomain>
2023-02-07 15:56:58 +0100

    a class MyInt that inherits from int

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+
 """define a class Myint"""
 
 
 class MyInt(int):
-    """define a class"""
 
-    def __eq__(self, other): # the __eq__ method is automatically called to
-        #determine whether the objects are equal or not.
+    """define a class"""
+# the __eq__ method is automatically called to
+# determine whether the objects are equal or not.
+    def __eq__(self, other):
         return int(self) != int(other)
 
     def __ne__(self, other):
