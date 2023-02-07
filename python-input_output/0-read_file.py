ea2142e06f99f78cde6f08f05d675bc869e606be
root <root@DESKTOP-C0MK0L2.localdomain>
2023-02-07 19:27:17 +0100

    a function that reads a text file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""define a function that reads a text file"""
+
+
 def read_file(filename=""):
     """a function that reads a text file"""
+
     with open(filename, "r" ,encoding= "UTF8") as myfile:
         for i in myfile:
             print(i.strip())
