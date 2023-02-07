cdefaccec3090d0bf7a10fec1ea608f58336b61d
root <root@DESKTOP-C0MK0L2.localdomain>
2023-02-07 15:37:28 +0100

    a class Rectangle that inherits from BaseGeometry

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -37,9 +37,16 @@ class Rectangle(BaseGeometry):
     def __repr(self):
         return self.__str__()
 
-class Square(Rectangle):
-    """ Square Class """
+
+class Square(BaseGeometry):
+    """ a class Square that inherits from Rectangle"""
+
     def __init__(self, size):
-        """ instantiation with size """
+        self.integer_validator("size", size)
         self.__size = size
-        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
