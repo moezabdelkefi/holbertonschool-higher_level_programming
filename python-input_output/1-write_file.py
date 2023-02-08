1ff8ade3a776c4f4287c5fa78e1d8133b9e74b26
root <root@DESKTOP-C0MK0L2.localdomain>
2023-02-08 17:54:51 +0100

    a function that writes a string to a text file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """ return length of the text"""
     with open(filename, "r", encoding="UTF8") as myfile:
-        for i in text:
-            return len(text)
+        myfile.write(text)
+        return len(text)
