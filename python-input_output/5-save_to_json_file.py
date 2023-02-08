b7fc575118b75a586fa3a004c7c625ab919539a0
root <root@DESKTOP-C0MK0L2.localdomain>
2023-02-08 19:03:54 +0100

    a function that writes an Object to a text file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" define a function save_to_json_file"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ a function that writes an Object to a text file"""
+
+    with open(filename, 'w', encoding='utf8') as t:
+        t = json.dumps(my_obj)
+        return t
