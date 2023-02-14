c466a154287be75ffce70a9b85ca177736657e11
moezabdelkef <moezabdelkefi17@gmail.com>
2023-02-14 16:12:47 +0100

    returns the dictionary representation of a Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -115,3 +115,6 @@ class Rectangle(Base):
             self.x = args[3]
         if len(args) >= 5:
             self.y = args[4]
+
+    def to_dictionary(self):
+        return {'id': self.id, 'width': self.width, 'height': self.height, 'x': self.x, 'y': self.y}
