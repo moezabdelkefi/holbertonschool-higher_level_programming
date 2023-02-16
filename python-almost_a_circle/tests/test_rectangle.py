a9089fc4aa722babb1040c04b89e7494e24cd991
moezabdelkef <moezabdelkefi17@gmail.com>
2023-02-16 13:21:41 +0100

    testinng

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -1,11 +1,11 @@
 import unittest
 from models.rectangle import Rectangle
 
-class Test_rectangle(unittest.TestCase):
-    def test_rectangle(self):
-        r1 = Rectangle(1, 2)
-        self.assertIsInstance(r1, Rectangle)
+class TestRectangle(unittest.TestCase):
+    def test_rectangle_1(self):
+        r = Rectangle(1, 2)
+        self.assertIsInstance(r, Rectangle)
 
     def test_rectangle_2(self):
-        r2 = Rectangle(1, 2, 3)
-        self.assertIsInstance(r2, Rectangle)
+        r = Rectangle(1, 2, 3)
+        self.assertIsInstance(r, Rectangle)
