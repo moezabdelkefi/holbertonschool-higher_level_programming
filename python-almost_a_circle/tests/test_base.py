4d634511b456e94ce853727b64f6acb098c796b3
moezabdelkef <moezabdelkefi17@gmail.com>
2023-02-16 13:10:04 +0100

    testing

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -34,12 +34,11 @@ class TestBase(unittest.TestCase):
         dict = [{'id': 89, 'x': 0, 'y': 0, 'width': 1, 'height': 1}]
         self.assertEqual(Base.from_json_string('[{"id": 89, "x": 0, "y": 0, "width": 1, "height": 1}]'), dict)
 
-    class Test_rectangle(unittest.TestCase):
-        def test_rectangle(self):
-            r1 = Rectangle(1, 2)
-            self.assertIsInstance(r1, Rectangle)
+    def test_rectangle(self):
+        r1 = Rectangle(1, 2)
+        self.assertIsInstance(r1, Rectangle)
 
-        def test_rectangle_2(self):
-            r2 = Rectangle(1, 2, 3)
-            self.assertIsInstance(r2, Rectangle)
+    def test_rectangle_2(self):
+        r2 = Rectangle(1, 2, 3)
+        self.assertIsInstance(r2, Rectangle)
 
