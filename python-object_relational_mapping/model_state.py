0d291d151c2622f32fcf72a3d34d80b68874239f
moezabdelkef <moezabdelkefi17@gmail.com>
2023-03-24 16:29:45 +0100

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -15,6 +15,10 @@ Base = declarative_base()
 
 # define the State class, inheriting from Base
 class State(Base):
+    """
+    Write a script that deletes all State objects with a
+    name containing the letter a from the database hbtn_0e_6_usa
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
