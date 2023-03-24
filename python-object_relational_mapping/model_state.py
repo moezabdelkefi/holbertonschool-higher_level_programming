36aefaccf12792dfdcab365913f693f1a94aff9f
moezabdelkef <moezabdelkefi17@gmail.com>
2023-03-24 16:27:36 +0100

    First state model

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# create the SQLAlchemy engine to connect to the MySQL server
+engine = create_engine('mysql://localhost:3306')
+
+# create the declarative base instance
+Base = declarative_base()
+
+# define the State class, inheriting from Base
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+# create the table in the MySQL database
+Base.metadata.create_all(engine)
