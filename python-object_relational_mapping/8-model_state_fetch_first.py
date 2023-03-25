4a83c647dd43c0bec64d973270fa292567a55c72
moezabdelkef <moezabdelkefi17@gmail.com>
2023-03-25 02:45:01 +0100

    fix pycodestyle

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Write a script that prints the first State object from the database hbtn_0e_6_usa
+Write a script that prints the first
+State object from the database hbtn_0e_6_usa
 """
 
 import sys
