58e29223b2106b044f992b657756c458ef5e9c4b
moezabdelkef <moezabdelkefi17@gmail.com>
2023-03-25 02:34:26 +0100

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
 """Write a python file that contains
 the class definition of a State """
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine('mysql://localhost:3306')
 
 Base = declarative_base()
 
 
 class State(Base):
-    """
-    Write a script that deletes all State objects with a
-    name containing the letter a from the database hbtn_0e_6_usa
+    """from sqlalchemy import Column, Integer, String
+    from sqlalchemy.ext.declarative import declarative_base
+    Base = declarative_base()
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-
-Base.metadata.create_all(engine)
