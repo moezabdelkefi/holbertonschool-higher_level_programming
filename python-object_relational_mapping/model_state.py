7268bdb412a3ea43b052f9e31a0fea25b0fbd3f4
moezabdelkef <moezabdelkefi17@gmail.com>
2023-03-25 02:19:37 +0100

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,13 +7,10 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-# create the SQLAlchemy engine to connect to the MySQL server
 engine = create_engine('mysql://localhost:3306')
 
-# create the declarative base instance
 Base = declarative_base()
 
-# define the State class, inheriting from Base
 class State(Base):
     """
     Write a script that deletes all State objects with a
@@ -23,5 +20,4 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
-# create the table in the MySQL database
 Base.metadata.create_all(engine)
