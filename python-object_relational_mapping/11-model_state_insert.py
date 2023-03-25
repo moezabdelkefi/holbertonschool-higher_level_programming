f9feb834b53742ea467cd3661cc43c804a2b3a94
moezabdelkef <moezabdelkefi17@gmail.com>
2023-03-25 02:56:57 +0100

    a script that adds the State object “Louisiana” to the database hbtn_0e_6_usa

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
 """
-Script that prints the State object with the name passed as argument
-from the database hbtn_0e_6_usa
+adds the State object “Louisiana” to the database hbtn_0e_6_usa
 """
 
-import sys
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 from model_state import Base, State
+from sys import argv
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    connection = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(connection.format(username, password, db_name))
+
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
 
-    if state is None:
-        print('Not found')
-    else:
-        print(state.id)
+    print(new_state.id)
