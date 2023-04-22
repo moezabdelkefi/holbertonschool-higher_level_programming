167f2f4035217ef2a68067cb65a689776b6d9623
moezabdelkef <moezabdelkefi17@gmail.com>
2023-04-23 00:55:25 +0100

    a script that prints two arguments passed to it, in the following format

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-const [arg1 = '', arg2 = ''] = process.argv.slice(2);
+
+const arg1 = process.argv[2] || 'undefined';
+const arg2 = process.argv[3] || 'undefined';
 
 console.log(`${arg1} is ${arg2}`);
