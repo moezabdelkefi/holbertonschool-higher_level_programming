4b9af7ed6709cb511a6fb104e8d3db3820e3a76c
moezabdelkef <moezabdelkefi17@gmail.com>
2023-04-23 02:48:58 +0100

    a class Rectangle that defines a rectangle and create an instance method called print()

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || !Number.isInteger(w) || h <= 0 || !Number.isInteger(h)) {
+      return this;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    let row = '';
+    for (let i = 0; i < this.height; i++) {
+      row += 'X'.repeat(this.width);
+      if (i < this.height - 1) {
+        row += '\n';
+      }
+    }
+    console.log(row);
+  }
+}
+module.exports = Rectangle;
