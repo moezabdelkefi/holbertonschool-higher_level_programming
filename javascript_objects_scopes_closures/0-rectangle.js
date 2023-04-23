bacb151230f634497f3e346a6deedc1394b5f4db
moezabdelkef <moezabdelkefi17@gmail.com>
2023-04-23 02:26:39 +0100

    an empty class Rectangle that defines a rectangle

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
+
 class Rectangle {
-    constructor() {
-    }
-  }
-  
-  const r1 = new Rectangle();
-  console.log(r1);
-  console.log(Object.prototype.toString.call(r1));
-  
+  constructor () {}
+}
+
+const r1 = new Rectangle();
+console.log(r1);
+console.log(r1.constructor);
