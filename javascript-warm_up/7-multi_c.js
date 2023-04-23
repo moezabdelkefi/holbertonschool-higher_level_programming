767a9a34eea3377553e8cbe5212e304c3c7d7016
moezabdelkef <moezabdelkefi17@gmail.com>
2023-04-23 01:55:50 +0100

    a script that prints x times C is fun

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
+
 const num = parseInt(process.argv[2]);
 
-if (isNaN(num || num <= 0)) {
+if (isNaN(num)) {
   console.log('Missing number of occurrences');
+} else if (num <= 0) {
+  process.exit();
 } else {
   let message = '';
   for (let i = 0; i < num; i++) {
